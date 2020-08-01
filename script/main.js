d062091b752312c5efd11b8e59747040edf47a9c
Marius Robert <rwandarushyarobert@gmail.com>
2020-08-01 22:43:24 +0200

    added firebase to the contact page

---
+++ script/main.js
@@ -0,0 +1,47 @@
+// firebase initialization
+var firebaseConfig = {
+    apiKey: "AIzaSyBfxQeMSwL4grsWtMAzIaQf1_IdZhL4R2I",
+    authDomain: "my-brand-83866.firebaseapp.com",
+    databaseURL: "https://my-brand-83866.firebaseio.com",
+    projectId: "my-brand-83866",
+    storageBucket: "my-brand-83866.appspot.com",
+    messagingSenderId: "170892001881",
+    appId: "1:170892001881:web:cf6fd03f1d68a88a7ea748",
+    measurementId: "G-LBSP5XRY37"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var messageRef= firebase.database().ref('messages');
+
+document.getElementById('contactForm').addEventListener('submit',submitForm);
+
+function submitForm(e){
+e.preventDefault();
+
+var name=getFormValues('name');
+var email=getFormValues('email');
+var message=getFormValues('message');
+saveMessage(name,email,message);
+document.querySelector('.alert').style.display='block';
+setTimeout(function(){
+    document.querySelector('.alert').style.display='none'; 
+},3000);
+document.getElementById('contactForm').reset();
+}
+
+function getFormValues(id){
+return document.getElementById(id).value;
+}
+
+
+// save message to firebase
+
+function saveMessage(name, email, message){
+var newMessageRef= messageRef.push();
+newMessageRef.set({
+name:name,
+email:email,
+message:message
+});
+}
