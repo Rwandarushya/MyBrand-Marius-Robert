62fc2ca5e914c026221cd699b8c1f6aa108ea1d8
Marius Robert <rwandarushyarobert@gmail.com>
2020-08-04 08:12:30 +0200

    changed realtime database to firestore

--- script/main.js
+++ script/main.js
@@ -1,18 +1,17 @@
 // firebase initialization
 var firebaseConfig = {
-    apiKey: "AIzaSyBfxQeMSwL4grsWtMAzIaQf1_IdZhL4R2I",
-    authDomain: "my-brand-83866.firebaseapp.com",
-    databaseURL: "https://my-brand-83866.firebaseio.com",
-    projectId: "my-brand-83866",
-    storageBucket: "my-brand-83866.appspot.com",
-    messagingSenderId: "170892001881",
-    appId: "1:170892001881:web:cf6fd03f1d68a88a7ea748",
-    measurementId: "G-LBSP5XRY37"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  var messageRef= firebase.database().ref('messages');
+  apiKey: "AIzaSyBfxQeMSwL4grsWtMAzIaQf1_IdZhL4R2I",
+  authDomain: "my-brand-83866.firebaseapp.com",
+  databaseURL: "https://my-brand-83866.firebaseio.com",
+  projectId: "my-brand-83866",
+  storageBucket: "my-brand-83866.appspot.com",
+  messagingSenderId: "170892001881",
+  appId: "1:170892001881:web:cf6fd03f1d68a88a7ea748",
+  measurementId: "G-LBSP5XRY37"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
 document.getElementById('contactForm').addEventListener('submit',submitForm);
 
@@ -38,10 +37,15 @@ return document.getElementById(id).value;
 // save message to firebase
 
 function saveMessage(name, email, message){
-var newMessageRef= messageRef.push();
-newMessageRef.set({
-name:name,
-email:email,
-message:message
+  db.collection("contacts").add({
+    name: name,
+    email: email,
+    message: message
+})
+.then(function(docRef) {
+     console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
 });
 }
