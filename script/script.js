b1554b09e9984a459012c1695dc37887391a483c
Marius Robert <rwandarushyarobert@gmail.com>
2020-08-24 22:21:43 +0200

    added animation to homepage

--- script/script.js
+++ script/script.js
@@ -17,24 +17,9 @@ const navSlide=() =>{
         });
         // burger animation
         burger.classList.toggle('toggle');
-    });
-
-    
+    });       
 }
 
 navSlide();
 
-function updateTransition() {
-    var el = document.querySelector("div.box");
-     
-    if (el) {
-      el.className = "box1";
-    } else {
-      el = document.querySelector("div.box1");
-      el.className = "box";
-    }
-     
-    return el;
-  }
-  
-  var intervalID = window.setInterval(updateTransition, 7000);
+
